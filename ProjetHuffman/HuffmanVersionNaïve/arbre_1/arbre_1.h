fca964fab0ae1248667dfadffde9c362a72a0d30
Sugitha-Nadarajah <73737469+Sugitha-Nadarajah@users.noreply.github.com>
2020-12-06 23:38:24 +0100

    Update arbre_1.h

--- ProjetHuffman/HuffmanVersionNaïve/arbre_1/arbre_1.h
+++ ProjetHuffman/HuffmanVersionNaïve/arbre_1/arbre_1.h
@@ -1,18 +1,30 @@
 
 
-#ifndef ARBRE_1
-#define ARBRE_1
+#ifndef arbre_1_h
+#define arbre_1_h
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX_INPUT 150000
+#define MAX_CODE 10000
 
 
+typedef struct MinNode {
+    struct HuffNode** tab;
+    int size;
+    int cap;
+}MinNode;
 
-struct HuffNode* createHuffNode(char ch, int w);
-static HuffNode* insert_node(struct HuffNode* ht, struct HuffNode* s);
-struct HuffNode* removeLastHuffNode(struct HuffNode* ht);
-struct HuffNode* createTree(struct HuffNode* head);
+struct HuffNode* newnewNode(char cc, int ww);
+void minimize(struct MinNode* minNode, int pos);
+struct HuffNode* get_minNode(struct MinNode* minNode);
+void insert_minNode(struct MinNode* minNode, struct HuffNode* parent);
+struct MinNode* create_list_minNode(struct HuffNode* L);
+struct HuffNode* creer_HuffmanTree(struct HuffNode* L);
+void display_tree(struct HuffNode* tree);
+void free_hufftree(struct HuffNode* ht);
 
-#endif //
+
+#endif /* arbre_1_h */
