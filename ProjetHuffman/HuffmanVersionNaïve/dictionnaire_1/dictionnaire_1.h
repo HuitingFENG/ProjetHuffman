e94b0443e96c5689f768fe8a2dc9262ab747eefe
Sugitha-Nadarajah <73737469+Sugitha-Nadarajah@users.noreply.github.com>
2020-12-06 23:40:35 +0100

    Update dictionnaire_1.h

--- ProjetHuffman/HuffmanVersionNaïve/dictionnaire_1/dictionnaire_1.h
+++ ProjetHuffman/HuffmanVersionNaïve/dictionnaire_1/dictionnaire_1.h
@@ -1,32 +1,19 @@
-
-#ifndef DICTIONNAIRE_1
-#define DICTIONNAIRE_1
+#ifndef dictionnaire_1_h
+#define dictionnaire_1_h
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX_INPUT 150000
+#define MAX_CODE 10000
 
 
-struct MinHeap {
-	int size;
-	int capacity;
-	struct HuffNode** array;
-};
-struct HuffNode* newnewNode(char ch, int w);
-struct MinHeap* createMinHeap(int capacity);
-void changerHuffNode(struct HuffNode** a, struct HuffNode** b);
-void minHeapify(struct MinHeap* minHeap, int idx);
-int isSizeOne(struct MinHeap* minHeap);
-struct HuffNode* extractMin(struct MinHeap* minHeap);
-void insertMinHeap(struct MinHeap* minHeap, struct HuffNode* HuffNode);
-void buildMinHeap(struct MinHeap* minHeap);
-void printArr(int arr[], int n);
-int isLeaf(struct HuffNode* root);
-struct MinHeap* createAndBuildMinHeap(char ch[], int w[], int size);
-struct HuffNode* buildHuffmanTree(char ch[], int w[], int size);
-void printCodes(struct HuffNode* root, int arr[], int top);
-void HuffmanCodes(char ch[], int w[], int size);
-void write_dictonnaire(struct HuffNode* L);
 
-#endif //
+void printCodes(struct HuffNode* root, int arr[], int parent, FILE* fp);
+void HuffmanCodes(struct HuffNode* L, FILE* fp);
+void write_dictonnaire(struct HuffNode* L, char* dico);
+
+
+
+#endif /* dictionnaire_1_h */
